9bae4e8fda67f594b9eb40b49e73765c0a8a8ae4
shoaibshaikh2046 <55142932+shoaibshaikh2046@users.noreply.github.com>
2020-11-26 22:33:07 +0530

    Commented UI test

--- test/TechFabricSln.Test/SeleniumTest.cs
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -14,21 +14,21 @@ namespace TechFabricSln.Test
     {
 
 
-        [Test]
-        [Category("UITests")]
-        public void VisitMicrosoft_CheckWindowsMenu()
-        {
-            ChromeOptions options = new ChromeOptions();
-            options.AddExcludedArgument("enable-automation");
-            options.AddArguments("--no-sandbox", "--incognito", "--start-maximized", "--disable-gpu", "--disable-extensions", "--disable-dev-shm-usage");
-            options.AddArgument("--headless");
-            IWebDriver driver = new ChromeDriver(options);
-            driver.Navigate().GoToUrl("https://www.microsoft.com/");
-            Thread.Sleep(10000);
-            string Windows_text = driver.FindElement(By.Id("shellmenu_2")).Text;
-            Assert.AreEqual("Windows", Windows_text);
-            driver.Quit();
-        }
+        //[Test]
+        //[Category("UITests")]
+        //public void VisitMicrosoft_CheckWindowsMenu()
+        //{
+        //    ChromeOptions options = new ChromeOptions();
+        //    options.AddExcludedArgument("enable-automation");
+        //    options.AddArguments("--no-sandbox", "--incognito", "--start-maximized", "--disable-gpu", "--disable-extensions", "--disable-dev-shm-usage");
+        //    options.AddArgument("--headless");
+        //    IWebDriver driver = new ChromeDriver(options);
+        //    driver.Navigate().GoToUrl("https://www.microsoft.com/");
+        //    //Thread.Sleep(10000);
+        //    //string Windows_text = driver.FindElement(By.Id("shellmenu_2")).Text;
+        //    //Assert.AreEqual("Windows", Windows_text);
+        //    driver.Quit();
+        //}
 
     }
 
