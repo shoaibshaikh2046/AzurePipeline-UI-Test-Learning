a0e301315ee5fcbff60dc0321f9075d20d80e05e
shoaibshaikh2046 <55142932+shoaibshaikh2046@users.noreply.github.com>
2020-11-26 09:49:19 +0530

    Added Selenium Test

---
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TechFabricSln.Test
+{
+    public class SeleniumTest
+    {
+
+
+        [Test]
+        [Category("UITests")]
+        public void VisitMicrosoft_CheckWindowsMenu()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.microsoft.com/");
+            Thread.Sleep(10000);
+            string Windows_text = driver.FindElement(By.Id("shellmenu_2")).Text;
+            Assert.AreEqual("Windows", Windows_text);
+            driver.Quit();
+        }
+
+    }
+
+}
